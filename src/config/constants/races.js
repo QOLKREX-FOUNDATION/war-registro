fd3a50b361bc6e965617a1cd22ab948830e27f66
qolkrex <qolkrex@gmail.com>
2026-01-29 12:52:07 -0500

    agregamos el contrato MONO

--- src/config/constants/races.js
+++ src/config/constants/races.js
@@ -4,6 +4,7 @@ import RABBIT from "../../../public/Json/race/Rabbits.json";
 import MACAW from "../../../public/Json/race/Macaw.json";
 import FERRET from "../../../public/Json/race/Ferret.json";
 import GUINEAPIG from "../../../public/Json/race/GuineaPig.json";
+import MONKEY from "../../../public/Json/race/Monkey.json";
 
 export const racesJson = {
   DOG,
@@ -12,4 +13,5 @@ export const racesJson = {
   MACAW,
   FERRET,
   GUINEA_PIG: GUINEAPIG,
+  MONKEY,
 };
